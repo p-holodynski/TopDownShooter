75f6533549805db2da14325922af13524a5d173b
phjoker <phjok3r@gmail.com>
2018-02-16 23:12:53 +0000

    EnemySpawner
    Spawner added with waves. Enemy is a prefab now

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent (typeof (NavMeshAgent))]
 public class EnemyMovement : LivingEntity 
 {
 
@@ -10,15 +11,31 @@ public class EnemyMovement : LivingEntity
 	//PlayerHealth playerHealth;
 	//EnemyHealth enemyHealth;
 	NavMeshAgent nav; // reference to navMeshAgent
+	Material skinMaterial;
+	Color originalColor;
 
+	//float attackDistanceTreshhold = .5f;
+	float timeBetweenAttacks = 1;
+	float nextAttackTime;
+	float myCollisionRadius;
+	float playerCollisionRadius;
+
+	public enum State {Idle, Chasing, Attacking};
+	State currentState;
 
 	protected override void Start()
 	{
 		base.Start ();
+		nav = GetComponent<NavMeshAgent>();
+		skinMaterial = GetComponent<Renderer> ().material;
+		originalColor = skinMaterial.color;
+
+		currentState = State.Chasing;
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
 		//playerHealth = player.GetComponent<PlayerHealth>();
 		//enemyHealth = GetComponent<EnemyHealth>();
-		nav = GetComponent<NavMeshAgent>();
+		//myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+		//playerCollisionRadius = GetComponent<SphereCollider> ().radius;
 
 		StartCoroutine (UpdatePath ());
 	}
@@ -35,18 +52,53 @@ public class EnemyMovement : LivingEntity
 //		{
 //			nav.enabled = false;
 //		}
+//		if (Time.time > nextAttackTime) {
+////			float squareDistanceToTarget = (player.position - transform.position).sqrMagnitude;
+////			if (squareDistanceToTarget < Mathf.Pow (attackDistanceTreshhold + myCollisionRadius + playerCollisionRadius, 2)) {
+////				
+////			}
+//			nextAttackTime = Time.time + timeBetweenAttacks;
+//			StartCoroutine (Attack ());
+//		}
 			
 	}
+
+//	IEnumerator Attack(){
+//
+//		currentState = State.Attacking;
+//		nav.enabled = false;
+//		Vector3 originalPosition = transform.position;
+//		Vector3 dirToTarget = (player.position - transform.position).normalized;
+//		Vector3 attackPosition = player.position - dirToTarget * (myCollisionRadius + playerCollisionRadius + attackDistanceTreshhold/2);
+//
+//		float attackSpeed = 3;
+//		float percent = 0;
+//
+//		while (percent <= 1) {
+//
+//			percent += Time.deltaTime * attackSpeed;
+//			float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
+//			transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+//
+//			yield return null;
+//		}
+//
+//		currentState = State.Chasing;
+//		nav.enabled = true;
+//	}
 
 	IEnumerator UpdatePath(){
 		float refreshRate = .25f;
 
 		while (player != null) {
-			//Vector3 playerPosition = new Vector3 (player.position.x, 0, player.position.z);
-			if (!dead) {
-				nav.SetDestination (player.position);
+			if (currentState == State.Chasing) {
+				//Vector3 dirToTarget = (player.position - transform.position).normalized;
+				//Vector3 playerPosition = player.position - dirToTarget * (myCollisionRadius + playerCollisionRadius + attackDistanceTreshhold/2);
+				if (!dead) {
+					nav.SetDestination (player.position);
+				}
+				yield return new WaitForSeconds (refreshRate);
 			}
-			yield return new WaitForSeconds (refreshRate);
 		}
 	}
 }
